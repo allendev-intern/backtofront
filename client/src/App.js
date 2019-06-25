ebb17eb35974c67ebd2a6d5145db0d649b5b56a3
allendev-intern <allendev.intern@gmail.com>
2019-06-25 19:13:53 +0800

    Added route and Form component for Adding Experience

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
+import AddExperience from "./components/profile-form/AddExperience";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
@@ -48,6 +49,11 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
               />
             </Switch>
           </section>
